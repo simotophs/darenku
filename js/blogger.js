8d73b7362ae91a9b542b5501f8d7aaa9faaf5b55
jonhal <yoliny_jonhal_2006@aliyun.com>
2013-04-26 11:56:17 +0800

    drk

---
+++ js/blogger.js
@@ -0,0 +1,48 @@
+	var OL_blogger = {
+
+		setViewportHeight:function(vw){
+			this.ht = $("#header").height();
+			this.hb = $("#footer").height();
+			this.h = document.documentElement.clientHeight;
+			this.vh = this.h-this.ht-this.hb;
+			if(vw)
+				$(vw).css("height",this.vh+"px");
+			else
+				$("#viewport").css("height",this.vh+"px");
+		},
+		setItemsSize:function(){
+			this.setViewportHeight();
+			var itemH = Math.round((this.vh-50)/4);
+			var itemW = Math.round(itemH*27/13);
+			$(".column").css("width",itemW+10+"px");
+			$(".column_x2").css("width",itemW*2+20+"px");
+			$(".items").css({width:itemW+"px",height:itemH+"px"});
+			$(".items_x2").css({width:itemW+"px",height:itemH+"px"});
+			$(".items_x2_c").css("width",itemW+20+"px")
+			$(".items_x2_i").css({width:itemW/2-5+"px",height:itemW/2-5+"px"})
+			$(".items_x4").css({width:itemW+"px",height:itemW+"px"});
+			$(".items_x8").css({width:itemW*2+10+"px",height:itemW+"px"});
+			$("#wall").css("width",$(".column").length*(itemW+10)+$(".column_x2").length*(itemW*2+20)+"px");
+		},
+		setRow:function(n,r){
+			/*
+			n: line
+			r: width/height
+			*/   
+			this.setViewportHeight();
+			var n = n || 3;
+			var r = r || 1;
+			var itemH = Math.round((this.vh-(n+1)*10)/n);
+			$(".column").css("width",itemH*r+10+"px");
+			$(".items").css({width:itemH*r+"px",height:itemH+"px"});
+			$("#wall").css("width",$(".column").length*(itemH*r+10)+"px");
+		},
+		setWallWidth:function(vw){
+			this.setViewportHeight(vw);
+			var itemW = $("#wall").children().width();
+			$("#wall").css("width",$(".column").length*(itemW)+"px");
+		}
+		
+
+	}
+	
